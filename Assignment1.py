d11f473b801fb20a8373f727d67c86ac762841cc
Ankur <64908229+Ankur-72@users.noreply.github.com>
2020-09-27 16:39:53 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,21 @@
+#text("Hey there!")
+#text("What's your name?")
+#name = input(str())
+#text("Hey " + name)
+print("Please enter the range of your numbers")
+print("Starting point:")
+#start = input(int())
+start = int(input())
+print("Ending point:")
+#end = input(int())
+end = int(input())
+
+print("The following numbers are even numbers within these limits:")
+for x in range(start,end,1):
+	if x%2 == 0:
+		print(x)
+
+print("The following numbers are odd numbers within these limits:")
+for x in range(start,end,1):
+	if x%2:
+		print(x)
